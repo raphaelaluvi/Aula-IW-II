9e662a0617dee9f0cd2754a69b8bd65d1e9ae1b8
raphaelaluvi <raphaluvi@gmail.com>
2024-02-22 19:58:29 -0300

    deu certoo

--- aula-02/js/script.js
+++ aula-02/js/script.js
@@ -3,6 +3,7 @@ const desligar = document.getElementById('desligar')
 const lampada = document.getElementById('lamp')
 const texto = document.getElementById('texto')
 const botaoDuplo = document.getElementById('botaoDuplo')
+const restaurar = document.getElementById('restaurar')
 
 function estaQuebrada(){
     return lampada.src.indexOf('quebrada') > -1
@@ -27,9 +28,35 @@ function lampQuebrada(){
     texto.innerHTML = "QUEBRADA"
 }
 
+function estadoInicial(){
+    return lampada.src.indexOf('desligada') > -1
+}
+
+function botaoLouD(){
+    if(!estaQuebrada()){
+        if(estadoInicial()){
+            lampada.src = "img/ligada.jpg"
+            texto.innerHTML = "LIGADA"
+        }
+        else{
+            lampada.src = "img/desligada.jpg"
+            texto.innerHTML = "DESLIGADA"
+        }
+       
+    }
+}
+
+function restaura(){
+    if(estaQuebrada()){
+        lampada.src = "img/desligada.jpg"
+        texto.innerHTML = "DESLIGADA"
+    }
+}
 
 ligar.addEventListener('click', lampLigada)
 desligar.addEventListener('click', lampDesligada)
+restaurar.addEventListener('click', restaura)
+botaoDuplo.addEventListener('click', botaoLouD)
 
 lampada.addEventListener('mouseover', lampLigada)
 lampada.addEventListener('mouseleave', lampDesligada)
