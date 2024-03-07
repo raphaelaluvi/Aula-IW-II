09775156be5f9a7a8bf33bc96f963b3c97021637
raphaelaluvi <raphaluvi@gmail.com>
2024-03-07 19:08:54 -0300

    metade certa

--- aula-04/js/imc.js
+++ aula-04/js/imc.js
@@ -1,42 +1,66 @@
-
-
-
-
 function diagnostico(imc){
     var genero = document.getElementById('genero').value
     if(genero == 'masc'){
         if (imc<20){
-            diagnostico.innerHTML = 'Diagnóstico:'
+            diag.innerHTML = 'Diagnóstico:'
             valorDiag.innerHTML = 'Abaixo do peso normal'
         }
         else if(20<=imc<25){
-            diagnostico.innerHTML = 'Diagnóstico:'
+            diag.innerHTML = 'Diagnóstico:'
             valorDiag.innerHTML = 'Normal'
         }
         else if(25<=imc<30){
-            diagnostico.innerHTML = 'Diagnóstico:'
+            diag.innerHTML = 'Diagnóstico:'
             valorDiag.innerHTML = 'Levemente obeso'
         }
         else if(30<=imc<35){
-            diagnostico.innerHTML = 'Diagnóstico:'
+            diag.innerHTML = 'Diagnóstico:'
             valorDiag.innerHTML = 'Obeso grau I'
         }
         else if(imc>=40){
-            diagnostico.innerHTML = 'Diagnóstico:'
+            diag.innerHTML = 'Diagnóstico:'
             valorDiag.innerHTML = 'Obeso grau III'
         }
         else{
-            diagnostico.innerHTML = 'Diagnóstico:'
+            diag.innerHTML = 'Diagnóstico:'
             valorDiag.innerHTML = 'Obeso grau II'
         }
     }
+    else if (genero == "fem"){
+        if(imc<19){
+            diag.innerHTML = 'Diagnóstico:'
+            valorDiag.innerHTML = 'Abaixo do peso normal'
+        }
+        else if(19<=imc<24){
+            diag.innerHTML = 'Diagnóstico:'
+            valorDiag.innerHTML = 'Normal'
+        }
+        else if(24<=imc<29){
+            diag.innerHTML = 'Diagnóstico:'
+            valorDiag.innerHTML = 'Levemente obeso'
+        }
+        else if(29<=imc<34){
+            diag.innerHTML = 'Diagnóstico:'
+            valorDiag.innerHTML = 'Obeso grau I'
+        }
+        else if(imc>=39){
+            diag.innerHTML = 'Diagnóstico:'
+            valorDiag.innerHTML = 'Obeso grau III'
+        }
+        else{
+            diag.innerHTML = 'Diagnóstico:'
+            valorDiag.innerHTML = 'Obeso grau II'
+        }
+    }
+    else{
+        diag.innerHTML = 'Sem dados suficientes'
+    }
 }
 
-function calcula(){
+function calcula(imc){
     var altura = parseFloat(document.getElementById('altura'). value)
     var peso = parseFloat(document.getElementById('peso'). value)
     var imc = peso/(altura*altura)
-    console.log('')
     resultado.innerHTML = 'Resultado:'
     valorIMC.innerHTML = 'Seu IMC é de '+ imc
     diagnostico()
